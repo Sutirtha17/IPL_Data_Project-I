f5bea3e3e2d469b05a0e84b9952e7fc9c28ae87e
Sutirtha Dey <sutirtha.dey.17@mountblue.tech>
2021-08-23 19:25:03 +0530

    modified the problem echonomicalBowlers.js

--- src/functions/economicalBowlers.js
+++ src/functions/economicalBowlers.js
@@ -31,15 +31,21 @@ function getTop10(objOfBowlers) {
 export default function economicalBowlers(result) {
   const mapOfBowlers = new Map();
   for (let i = 1; i < result.length; i++) {
-    if (result[i][0] <= 59) {
+    if (result[i][0] >= 518 && result[i][0] <= 576) {
       let bowler = result[i][8];
       let runsOnEachBowl = Number(result[i][17]);
-      if (mapOfBowlers.get(bowler) == undefined)
-        mapOfBowlers.set(bowler, [runsOnEachBowl, 1]);
-      else {
+      let wideBalls = Number(result[i][10]);
+      let noballs = Number(result[i][13]);
+      if (mapOfBowlers.get(bowler) == undefined) {
+        if (wideBalls > 0 || noballs > 0)
+          mapOfBowlers.set(bowler, [runsOnEachBowl, 0]);
+        else {
+          mapOfBowlers.set(bowler, [runsOnEachBowl, 1]);
+        }
+      } else {
         let currentStat = mapOfBowlers.get(bowler);
         currentStat[0] += runsOnEachBowl;
-        currentStat[1] += 1;
+        if (wideBalls == 0 && noballs == 0) currentStat[1] += 1;
         mapOfBowlers.set(bowler, currentStat);
       }
     }
