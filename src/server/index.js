39952e1256d0d86b498e5cef4e84c362ffd1d458
Sutirtha Dey <sutirtha.dey.17@mountblue.tech>
2021-08-24 16:23:13 +0530

    corrected the identation of ipl.js and index.js

--- src/server/index.js
+++ src/server/index.js
@@ -16,13 +16,19 @@ const deliveries = fs.createReadStream("./../data/deliveries.csv");
 papa.parse(matches, {
   complete: function (matches) {
     //problem 1
+
     const objectOfYears = numberOfMatches(matches.data);
+
     const jsonOfYears = JSON.stringify(objectOfYears);
+
     fs.writeFileSync("./../public/output/matchesPerYear.json", jsonOfYears);
 
     //problem 2
+
     const objectOfMatchesWonPerYear = matchesWonPerYear(matches.data);
+
     const jsonOfMatches = JSON.stringify(objectOfMatchesWonPerYear);
+
     fs.writeFileSync(
       "./../public/output/matchesWonPerYear.json",
       jsonOfMatches
@@ -31,8 +37,11 @@ papa.parse(matches, {
     papa.parse(deliveries, {
       complete: function (deliveries) {
         // problem 3
+
         const objOf2016 = extraRuns2016(matches.data, deliveries.data, 2016);
+
         const jsonExtraRuns = JSON.stringify(objOf2016);
+
         fs.writeFileSync(
           "./../public/output/extraRuns2016.json",
           jsonExtraRuns
@@ -45,6 +54,7 @@ papa.parse(matches, {
           deliveries.data,
           2015
         );
+
         const jsonOfBowlers = JSON.stringify(objOfbowlers);
         fs.writeFileSync(
           "./../public/output/economicalBowlers.json",
--- src/server/ipl.js
+++ src/server/ipl.js
@@ -1,87 +1,84 @@
-function bowlsToOverConvertion(totalBowls) {
-  return Math.floor(totalBowls / 6) + (totalBowls % 6) / 10;
-}
-
-function calculateEconomy(objectOfBowlers) {
-  const objOfBowlers = new Object();
-  for (let [bowler, currentStat] of Object.entries(objectOfBowlers)) {
-    let runs = currentStat[0];
-    let overs = bowlsToOverConvertion(currentStat[1]);
-    let economyRate = (runs / overs).toFixed(2);
-    objOfBowlers[bowler] = economyRate;
-  }
-  return objOfBowlers;
-}
-
-function getTop10(objOfBowlers) {
-  const resultObject = new Object();
-  let keysSorted = Object.keys(objOfBowlers).sort(function (a, b) {
-    return objOfBowlers[a] - objOfBowlers[b];
-  });
-
-  for (let i = 0; i < 10; i++) {
-    let bowler = keysSorted[i];
-    let economyRate = objOfBowlers[bowler];
-    resultObject[bowler] = economyRate;
-  }
-  return resultObject;
-}
+/* functions used to get matchIds for problem 3 and problem 4*/
 
 function getMatchIds(matches, requiredSeason) {
   let matchIds = new Set();
+
   for (let i = 1; i < matches.length; i++) {
-    if (matches[i][1] == requiredSeason) matchIds.add(matches[i][0]);
+    if (matches[i][1] == requiredSeason) {
+      matchIds.add(matches[i][0]);
+    }
   }
+
   return matchIds;
 }
 
+/* Problem 1*/
+
 function numberOfMatches(matches) {
   const objectOfYears = new Object();
+
   for (let i = 1; i < matches.length; i++) {
     let year = matches[i][1];
+
     if (objectOfYears[year] == undefined) {
       objectOfYears[year] = 1;
     } else {
       objectOfYears[year] = Number(objectOfYears[year]) + 1;
     }
   }
+
   return objectOfYears;
 }
 
+/* Problem 2*/
+
 function matchesWonPerYear(matches) {
-  const objectOfMatchesWonPerYear = new Object(); //
+  const objectOfMatchesWonPerYear = new Object();
+
   for (let i = 1; i < matches.length; i++) {
     let year = matches[i][1];
+
     let team = matches[i][10];
+
     if (objectOfMatchesWonPerYear[year] == undefined) {
       objectOfMatchesWonPerYear[year] = new Object();
     }
+
     if (objectOfMatchesWonPerYear[year][team] == undefined && team)
       objectOfMatchesWonPerYear[year][team] = 1;
     else {
       if (team) {
         let noOfMatches = Number(objectOfMatchesWonPerYear[year][team]);
+
         noOfMatches += 1;
         objectOfMatchesWonPerYear[year][team] = noOfMatches;
       }
     }
-    //if (team == "Kolkata Knight Riders" && year == "2008") console.log(team);
   }
+
   return objectOfMatchesWonPerYear;
 }
 
+/* Problem 3*/
+
 function extraRuns2016(matches, deliveries, requiredSeason) {
   const objOfExtraRuns2016 = new Object();
+
   let matchIds = getMatchIds(matches, requiredSeason);
+
   for (let i = 1; i < deliveries.length; i++) {
     let matchId = deliveries[i][0];
+
     if (matchIds.has(matchId)) {
       let team = deliveries[i][3];
+
       let extraRuns = Number(deliveries[i][16]);
-      if (objOfExtraRuns2016[team] == undefined)
+
+      if (objOfExtraRuns2016[team] == undefined) {
         objOfExtraRuns2016[team] = extraRuns;
-      else {
+      } else {
         let currentExtraRuns = Number(objOfExtraRuns2016[team]);
+
         currentExtraRuns += extraRuns;
         objOfExtraRuns2016[team] = currentExtraRuns;
       }
@@ -90,31 +87,85 @@ function extraRuns2016(matches, deliveries, requiredSeason) {
   return objOfExtraRuns2016;
 }
 
+/* Problem 4*/
+
+function bowlsToOverConvertion(totalBowls) {
+  return Math.floor(totalBowls / 6) + (totalBowls % 6) / 10;
+}
+
+function calculateEconomy(objectOfBowlers) {
+  const objOfBowlers = new Object();
+
+  for (let [bowler, currentStat] of Object.entries(objectOfBowlers)) {
+    let runs = currentStat[0];
+
+    let overs = bowlsToOverConvertion(currentStat[1]);
+
+    let economyRate = (runs / overs).toFixed(2);
+
+    objOfBowlers[bowler] = economyRate;
+  }
+
+  return objOfBowlers;
+}
+
+function getTop10(objOfBowlers) {
+  const resultObject = new Object();
+
+  let keysSorted = Object.keys(objOfBowlers).sort(function (a, b) {
+    return objOfBowlers[a] - objOfBowlers[b];
+  });
+
+  for (let i = 0; i < 10; i++) {
+    let bowler = keysSorted[i];
+
+    let economyRate = objOfBowlers[bowler];
+
+    resultObject[bowler] = economyRate;
+  }
+
+  return resultObject;
+}
+
 function economicalBowlers(matches, deliveries, requiredSeason) {
   const objectOfBowlers = new Object();
+
   let matchIds = getMatchIds(matches, requiredSeason);
+
   for (let i = 1; i < deliveries.length; i++) {
     let matchId = deliveries[i][0];
+
     if (matchIds.has(matchId)) {
       let bowler = deliveries[i][8];
+
       let runsOnEachBowl = Number(deliveries[i][17]);
+
       let wideBalls = Number(deliveries[i][10]);
+
       let noballs = Number(deliveries[i][13]);
+
       if (objectOfBowlers[bowler] == undefined) {
-        if (wideBalls > 0 || noballs > 0)
+        if (wideBalls > 0 || noballs > 0) {
           objectOfBowlers[bowler] = [runsOnEachBowl, 0];
-        else {
+        } else {
           objectOfBowlers[bowler] = [runsOnEachBowl, 1];
         }
       } else {
         let currentStat = objectOfBowlers[bowler];
+
         currentStat[0] += runsOnEachBowl;
-        if (wideBalls == 0 && noballs == 0) currentStat[1] += 1;
+
+        if (wideBalls == 0 && noballs == 0) {
+          currentStat[1] += 1;
+        }
+
         objectOfBowlers[bowler] = currentStat;
       }
     }
   }
+
   const objOfBowlers = calculateEconomy(objectOfBowlers);
+
   return getTop10(objOfBowlers);
 }
 
